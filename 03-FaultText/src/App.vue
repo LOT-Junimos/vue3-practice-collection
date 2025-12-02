a7236748e2660db50844ab878abfe616e7a65543
LOT-Junimos <2066811360@qq.com>
2025-12-02 22:28:08 +0800

    Create App.vue

---
+++ 03-FaultText/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <FaultText />
+  </div>
+</template>
+
+<script setup>
+import FaultText from './components/FaultText.vue';
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+}
+</style>
