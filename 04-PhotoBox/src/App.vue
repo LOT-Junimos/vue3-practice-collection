774c20b5275a9cb3d4a57e21e16075da4b612d1e
LOT-Junimos <2066811360@qq.com>
2025-12-04 12:55:32 +0800

    Create App.vue

---
+++ 04-PhotoBox/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 引入照片墙组件 -->
+  <PhotoBox />
+</template>
+
+<script setup>
+// 导入PhotoBox组件
+import PhotoBox from './components/PhotoBox.vue'
+</script>
+
+<style scoped>
+/* 全局基础样式 */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  background-color: #171717;
+}
+</style>
