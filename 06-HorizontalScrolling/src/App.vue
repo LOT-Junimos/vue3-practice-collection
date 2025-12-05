1747067eca79b916c58a4c048474b0a57ad39fe1
LOT-Junimos <2066811360@qq.com>
2025-12-05 21:31:04 +0800

    Create App.vue

---
+++ 06-HorizontalScrolling/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+    <!-- 水平滚动图片组件 -->
+    <HorizontalScrolling />
+  </div>
+</template>
+
+<script setup>
+// 导入组件
+import HorizontalScrolling from './components/HorizontalScrolling.vue'
+</script>
+
+<style scoped>
+/* 根容器样式，可根据需要调整 */
+.app-container {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
