7d8a7b7b51ebb487c6a234d1ae2ab1af97df9933
LOT-Junimos <2066811360@qq.com>
2025-12-07 23:49:09 +0800

    Create App.vue

---
+++ 07-MagneticBall/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <MagneticBall />
+  </div>
+</template>
+
+<script setup>
+import MagneticBall from './components/MagneticBall.vue'
+</script>
+
+<style>
+/* 全局样式重置 */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+</style>
